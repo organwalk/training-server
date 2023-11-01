b929aa40349eb81ab946c5bf4c5d54b286283979
zhaozhifeng <1471573436@qq.com>
2023-11-01 16:33:08 +0800

    new

--- training-progress-service/src/main/java/com/training/progress/mapper/ProgressLessonMapper.java
+++ training-progress-service/src/main/java/com/training/progress/mapper/ProgressLessonMapper.java
@@ -1,5 +1,6 @@
 package com.training.progress.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -10,4 +11,10 @@ public interface ProgressLessonMapper {
 
     @Select("select over_chapter_sum = over_chapter_sum+1 from t_progress_lesson where lesson_id=#{lesson_id} and student_id=#{student_id}")
     Integer updateChapterSum(@Param("lesson_id")int lesson_id,@Param("student_id")int student_id);
+
+    @Insert("insert into t_progress_lesson(lesson_id, student_id, over_chapter_sum, lesson_chapter_sum) VALUES (#{lesson_id},#{student_id},#{over_chapter_sum},#{lesson_chapter_sum})")
+    Integer insertPorLesson(@Param("lesson_id")int lesson_id,@Param("student_id")int student_id,@Param("over_chapter_sum")int over_chapter_sum,@Param("lesson_chapter_sum")int lesson_chapter_sum);
+
+    @Select("select COUNT(id) from t_progress_lesson where lesson_id=#{lesson_id} and student_id=#{student_id}")
+    Integer judgeExitInTable(@Param("lesson_id")int lesson_id,@Param("student_id")int student_id);
 }
--- training-progress-service/src/main/java/com/training/progress/servier/ProgressService.java
+++ training-progress-service/src/main/java/com/training/progress/servier/ProgressService.java
@@ -1,7 +1,11 @@
 package com.training.progress.servier;
 
+import com.training.common.entity.DataRespond;
 import com.training.common.entity.MsgRespond;
 
 public interface ProgressService {
     MsgRespond MarkChapterComplete(int lesson_id,int chapter_id,int student_id);
+    MsgRespond insertStuLessonProgress(int lesson_id,int student_id,int over_chapter_sum,int lesson_chapter_sum);
+
+    DataRespond getStuAllByLessonId(int lesson_id, int page_size, int offset);
 }
