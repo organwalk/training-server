cca472c24dba84b431534c3aaa7d456ba8cb602e
zhaozhifeng <1471573436@qq.com>
2023-11-07 10:38:58 +0800

    new

--- training-resource/src/main/java/com/training/resource/config/WebClientConfig.java
+++ training-resource/src/main/java/com/training/resource/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.training.resource.config;
 
 import com.training.resource.client.DeptClient;
+import com.training.resource.client.TrainingClient;
 import com.training.resource.client.UserClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,5 +35,11 @@ public class WebClientConfig {
         return factory.createClient(UserClient.class);
     }
 
+    @Bean
+    public TrainingClient trainingClient(){
+        WebClient client = WebClient.builder().build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+        return factory.createClient(TrainingClient.class);
+    }
 
 }
