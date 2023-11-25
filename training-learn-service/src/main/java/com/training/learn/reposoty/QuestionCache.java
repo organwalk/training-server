73046126d91db18d50635fd8109ac086063923c0
organwalk <hinzink@foxmail.com>
2023-11-25 19:55:46 +0800

    2023-11-25 0.优化了教材资源删除业务 1.优化了试卷列表业务逻辑

--- training-learn-service/src/main/java/com/training/learn/reposoty/QuestionCache.java
+++ training-learn-service/src/main/java/com/training/learn/reposoty/QuestionCache.java
@@ -2,6 +2,7 @@ package com.training.learn.reposoty;
 
 
 import com.training.learn.entity.request.CacheReq;
+import com.training.learn.entity.result.StuQuestionResult;
 import com.training.learn.entity.result.TeaQuestionResult;
 import org.springframework.stereotype.Repository;
 
@@ -10,15 +11,19 @@ import java.util.List;
 @Repository
 public interface QuestionCache {
     //保存试题
-    void saveQuestion(int test_id, List<TeaQuestionResult.Question> question);
+    void saveTeaQuestion(int test_id, String type, List<TeaQuestionResult.Question> question);
+    void saveStuQuestion(int test_id, String type, List<StuQuestionResult.Question> question);
     //获取指定试题
-    List<TeaQuestionResult.Question> getQuestion(int test_id);
+    List<TeaQuestionResult.Question> getTeaQuestion(int test_id, String type);
+    List<StuQuestionResult.Question> getStuQuestion(int test_id, String type);
     //删除指定试题
     void deleteQuestion(int test_id);
+    void deleteStuQuestion(int test_id);
     //保存暂时编写的试题
     void saveCache(int test_id, CacheReq req);
-    //删除暂时编写的试题
+    //获取暂时编写的试题
     List<CacheReq.Question> getCache(int test_id);
     //判断指定测试的缓存是否存在
     boolean judgeKeyExit(int test_id);
+    boolean judgeStuKeyExit(int test_id);
 }
