2fd89574529e9a1639e4ac163cbb15347a246927
organwalk <hinzink@foxmail.com>
2023-11-28 20:44:57 +0800

    2023-11-28 0.推送服务初步

--- training-push-service/src/main/java/com/push/server/WebSocketServer.java
+++ training-push-service/src/main/java/com/push/server/WebSocketServer.java
@@ -1,102 +1,78 @@
 package com.push.server;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.push.entity.PushMessage;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-@ServerEndpoint(value = "/imserver/{username}")
+@ServerEndpoint(value = "/push/{uid}")
 @Component
 public class WebSocketServer {
+    private static final Logger logger = LogManager.getLogger(WebSocketServer.class);
 
-    private static final Logger log = LoggerFactory.getLogger(WebSocketServer.class);
-
-    /**
-     * 记录当前在线连接数
-     */
-    public static final Map<String, Session> sessionMap = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<String, Session> sessionMap = new ConcurrentHashMap<>();
 
     /**
-     * 连接建立成功调用的方法
+     * 连接建立成功时缓存连接状态
      */
     @OnOpen
-    public void onOpen(Session session, @PathParam("username") String username) {
-        sessionMap.put(username, session);
-        log.info("有新用户加入，username={}, 当前在线人数为：{}", username, sessionMap.size());
-        JSONObject result = new JSONObject();
-        JSONArray array = new JSONArray();
-        result.put("users", array);
-        for (Object key : sessionMap.keySet()) {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("username", key);
-            // {"username", "zhang", "username": "admin"}
-            array.add(jsonObject);
-        }
-//        {"users": [{"username": "zhang"},{ "username": "admin"}]}
-        sendAllMessage(JSONObject.toJSONString(result));  // 后台发送消息给所有的客户端
+    public void onOpen(@PathParam("uid") String uid, Session session) {
+        sessionMap.put(uid, session);
+        logger.info("用户uid={}上线, 当前服务在线人数为：{}", uid, sessionMap.size());
     }
 
     /**
      * 连接关闭调用的方法
      */
     @OnClose
-    public void onClose(Session session, @PathParam("username") String username) {
-        sessionMap.remove(username);
-        log.info("有一连接关闭，移除username={}的用户session, 当前在线人数为：{}", username, sessionMap.size());
+    public void onClose(@PathParam("uid") String uid) {
+        sessionMap.remove(uid);
+        logger.info("用户uid={}下线，已移除其连接状态, 当前在线人数为：{}", uid, sessionMap.size());
     }
 
-    /**
-     * 收到客户端消息后调用的方法
-     * 后台收到客户端发送过来的消息
-     * onMessage 是一个消息的中转站
-     * 接受 浏览器端 socket.send 发送过来的 json数据
-     * @param message 客户端发送过来的消息
-     */
+
     @OnMessage
-    public void onMessage(String message, Session session, @PathParam("username") String username) {
-        log.info("服务端收到用户username={}的消息:{}", username, message);
-        JSONObject obj = JSON.parseObject(message);
-        String toUsername = obj.getString("to"); // to表示发送给哪个用户，比如 admin
-        String text = obj.getString("text"); // 发送的消息文本  hello
-        // {"to": "admin", "text": "聊天文本"}
-        System.out.println(sessionMap);
-        Session toSession = sessionMap.get(toUsername); // 根据 to用户名来获取 session，再通过session发送消息文本
-        if (toSession != null) {
-            // 服务器端 再把消息组装一下，组装后的消息包含发送人和发送的文本内容
-            // {"from": "zhang", "text": "hello"}
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("from", username);  // from 是 zhang
-            jsonObject.put("text", text);  // text 同上面的text
-            this.sendMessage(jsonObject.toString(), toSession);
-            log.info("发送给用户username={}，消息：{}", toUsername, jsonObject.toString());
-        } else {
-            log.info("发送失败，未找到用户username={}的session", toUsername);
+    public void onMessage(String message, @PathParam("uid") String uid) {
+        logger.info("服务端收到用户uid={}的消息:{}", uid, message);
+        PushMessage pushMessage = JSON.parseObject(message).toJavaObject(PushMessage.class);
+        try (ExecutorService executorService = Executors.newFixedThreadPool(10)) {
+            pushMessage.getTo_uid_list().forEach(to_uid -> {
+                Session session = sessionMap.get(to_uid);
+                if (Objects.nonNull(session)) {
+                    executorService.submit(() -> sendMessageToUidList(sessionMap.get(to_uid), pushMessage));
+                }
+            });
+            executorService.shutdown();
         }
     }
 
     @OnError
-    public void onError(Session session, Throwable error) {
-        log.error("发生错误");
+    public void onError(Throwable error) {
+        logger.error("发生错误");
         error.printStackTrace();
     }
 
     /**
      * 服务端发送消息给客户端
      */
-    private void sendMessage(String message, Session toSession) {
+    private void sendMessageToUidList(Session session, PushMessage pushMessage) {
+        String message = pushMessage.getMessage();
         try {
-            log.info("服务端给客户端[{}]发送消息{}", toSession.getId(), message);
-            toSession.getBasicRemote().sendText(message);
+            logger.info("服务端为客户端[{}]推送消息{}", session.getId(), message);
+            session.getBasicRemote().sendText(message);
         } catch (Exception e) {
-            log.error("服务端发送消息给客户端失败", e);
+            logger.error("服务端消息推送失败", e);
         }
     }
 
@@ -104,14 +80,13 @@ public class WebSocketServer {
      * 服务端发送消息给所有客户端
      */
     private void sendAllMessage(String message) {
-        try {
-            for (Session session : sessionMap.values()) {
-                log.info("服务端给客户端[{}]发送消息{}", session.getId(), message);
+        sessionMap.values().forEach(session -> {
+            try {
                 session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            log.error("服务端发送消息给客户端失败", e);
-        }
+        });
     }
 }
 
--- training-resource/src/main/java/com/training/resource/config/RedisConfig.java
+++ training-resource/src/main/java/com/training/resource/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.training.resource.config;
 
+import com.mysql.cj.Session;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
